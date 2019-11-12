c5c2b82924bca33b212706a0c69a50e699fc9421
Amanda <amandaralves@live.com>
2019-11-12 01:29:28 -0300

    Página de listagem e busca de músicas pronta!

--- src/pages/musicas/musicas.ts
+++ src/pages/musicas/musicas.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ApplicationProvider } from "../../providers/application/application";
 
 @Component({
   selector: 'page-musicas',
   templateUrl: 'musicas.html'
 })
 export class MusicasPage {
+  songs: any;
+  searchQuery: string = '';
+  items: string[];
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private appProvider: ApplicationProvider) {
   }
-  
+
+  ngOnInit() {
+    this.appProvider.getMusicas().subscribe(res => this.songs = res)
+  }
+
+  initializeItems() {
+    setTimeout( () => {
+      this.appProvider.getMusicas().subscribe(res => this.songs = res)
+    }, 5000);
+
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    const val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.songs = this.songs.filter((song) => {
+        return (song.mscNome.toString().toLowerCase().indexOf(val.toString().toLowerCase()) > -1);
+      })
+    }
+  }
+
+
 }
